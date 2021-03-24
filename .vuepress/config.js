5bbfeecb44223ac8e2fd745d13fc88efdb963355
米梦宇 <mimengyu@autohome.com.cn>
2021-03-24 18:55:54 +0800

    feat: 切换到原主题

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,85 +1,11 @@
 module.exports = {
   title: "Mengyu'Notes",
-  description: 'A simple and beautiful vuepress blog theme .',
-  dest: 'public',
-  head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }],
-    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
-  ],
-  theme: 'reco',
+  description: "基于云开发的 VuePress 网站应用示例",
+  base: "/vuepress/",
   themeConfig: {
     nav: [
-      { text: 'Home', link: '/', icon: 'reco-home' },
-      { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
-      { text: 'Docs', 
-        icon: 'reco-message',
-        items: [
-          { text: 'vuepress-reco', link: '/docs/theme-reco/' }
-        ]
-      },
-      { text: 'Contact', 
-        icon: 'reco-message',
-        items: [
-          { text: 'GitHub', link: 'https://github.com/recoluan', icon: 'reco-github' }
-        ]
-      }
+      { text: "首页", link: "/" },
+      { text: "指南", link: "/guides/" },
     ],
-    sidebar: {
-      '/docs/theme-reco/': [
-        '',
-        'theme',
-        'plugin',
-        'api'
-      ]
-    },  
-    type: 'blog',
-    // 博客设置
-    blogConfig: {
-      category: {
-        location: 2, // 在导航栏菜单中所占的位置，默认2
-        text: 'Category' // 默认 “分类”
-      },
-      tag: {
-        location: 3, // 在导航栏菜单中所占的位置，默认3
-        text: 'Tag' // 默认 “标签”
-      }
-    },
-    logo: '/logo.png',
-    // 搜索设置
-    search: false,
-    searchMaxSuggestions: 10,
-    // 自动形成侧边导航
-    // sidebar: 'auto',
-    // 最后更新时间
-    lastUpdated: 'Last Updated',
-    // 作者
-    author: '@mengyu',
-    // 作者头像
-    authorAvatar: '/avatar.png',
-    // 备案号
-    record: 'xxxx',
-    // 项目开始时间
-    startYear: '2021'
-    /**
-     * 密钥 (if your blog is private)
-     */
-
-    // keyPage: {
-    //   keys: ['your password'],
-    //   color: '#42b983',
-    //   lineColor: '#42b983'
-    // },
-
-    /**
-     * valine 设置 (if you need valine comment )
-     */
-
-    // valineConfig: {
-    //   appId: '...',// your appId
-    //   appKey: '...', // your appKey
-    // }
   },
-  markdown: {
-    lineNumbers: true
-  }
-}  
+};
